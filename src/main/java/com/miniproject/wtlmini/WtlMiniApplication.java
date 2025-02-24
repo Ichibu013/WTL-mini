b30a94bd32e6905868bdd9e8603c455a4d7aac27
Ansh Sharma <ansharma013@gmail.com>
2025-02-24 17:05:38 +0530

    Spring Initialize

---
+++ src/main/java/com/miniproject/wtlmini/WtlMiniApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.wtlmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtlMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtlMiniApplication.class, args);
+    }
+
+}
